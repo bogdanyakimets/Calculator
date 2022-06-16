3ec1f5be8be8678b2edb62d1ee42fc9eba276876
bogdanyakimets <105966530+bogdanyakimets@users.noreply.github.com>
2022-06-17 00:16:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿decimal num1 = 0;
+decimal num2 = 0;
+char symbol;
+
+
+while (true)
+{
+    Console.WriteLine("Possible actions: +, -, *, /.");
+    Console.WriteLine("Write number:");
+    num1 = decimal.Parse(Console.ReadLine());
+
+    Console.WriteLine("Write actions:");
+    symbol = char.Parse(Console.ReadLine());
+
+    Console.WriteLine("Write number");
+    num2 = decimal.Parse(Console.ReadLine());
+
+    Console.WriteLine();
+
+    Console.Write("Result: ");
+
+    switch (symbol)
+    {
+        case '+':
+            Console.WriteLine(num1 + num2);
+            break;
+        case '-':
+            Console.WriteLine(num1 - num2);
+            break;
+        case '/':
+            Console.WriteLine(num1 / num2);
+            
+            break;
+        case '*': 
+            Console.WriteLine(num1 * num2);
+            break;
+        default: Console.WriteLine("Wrong format");
+            break;         
+    }
+    
+    Thread.Sleep(2500);
+    Console.Clear();
+}
